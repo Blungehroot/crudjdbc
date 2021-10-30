2230aaf2370ffb4ddcf6213a6f0ca1503062d47b
insight <insight2pizzza>
2021-10-30 19:11:59 +0300

    start impl writer

---
+++ src/main/java/com/crudjdbc/app/repository/jdbc/JdbcWriterRepositoryImpl.java
@@ -0,0 +1,33 @@
+package com.crudjdbc.app.repository.jdbc;
+
+import com.crudjdbc.app.model.Writer;
+import com.crudjdbc.app.repository.WriterRepository;
+
+import java.util.List;
+
+public class JdbcWriterRepositoryImpl implements WriterRepository {
+    @Override
+    public Writer getById(Integer integer) {
+        return null;
+    }
+
+    @Override
+    public List<Writer> getAll() {
+        return null;
+    }
+
+    @Override
+    public Writer save(Writer writer) {
+        return null;
+    }
+
+    @Override
+    public Writer update(Writer writer) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(Integer integer) {
+
+    }
+}
