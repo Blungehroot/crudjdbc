6d6edb5ea69837352c77b0d0c680b1701bbf12c7
Yevhenii Shaidur <YEVGENIY.SHAYDUR@kyivstar.net>
2021-11-03 15:40:52 +0200

    impl test

--- src/test/java/labelservicetests/LabelServiceTest.java
+++ src/test/java/labelservicetests/LabelServiceTest.java
@@ -1,16 +1,20 @@
 package labelservicetests;
 
 import com.crudjdbc.app.model.Label;
+import com.crudjdbc.app.repository.LabelRepository;
 import com.crudjdbc.app.repository.jdbc.JdbcLabelRepositoryImpl;
 import com.crudjdbc.app.service.LabelServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.mockito.Mockito.lenient;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class LabelServiceTest {
@@ -19,7 +23,7 @@ public class LabelServiceTest {
     private LabelServiceImpl labelServiceImpl;
 
     @Mock
-    private JdbcLabelRepositoryImpl jdbcLabelRepository;
+    private JdbcLabelRepositoryImpl labelRepository;
 
     @Test
     void saveLabel_shouldBeSuccess() {
@@ -27,10 +31,15 @@ public class LabelServiceTest {
         label.setId(1);
         label.setName("Test label");
 
-        lenient().when(jdbcLabelRepository.save(label)).thenReturn(label);
+        when(labelRepository.save(label)).thenReturn(label);
 
-        labelServiceImpl.save(label);
-        verify(jdbcLabelRepository).save(label);
+        labelRepository.save(label);
+        Label labelActual =labelServiceImpl.save(label);
+
+        assertNotNull(label);
+        assertEquals(label, labelActual);
+
+        verify(labelRepository).save(ArgumentMatchers.eq(label));
 
     }
 }
