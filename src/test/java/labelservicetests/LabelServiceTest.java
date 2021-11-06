d85a73a6fe3904b85aeb9ccd9d330378f4b4db12
Yevhenii Shaidur <YEVGENIY.SHAYDUR@kyivstar.net>
2021-11-06 23:09:58 +0200

    impl test

--- src/test/java/labelservicetests/LabelServiceTest.java
+++ src/test/java/labelservicetests/LabelServiceTest.java
@@ -1,7 +1,6 @@
 package labelservicetests;
 
 import com.crudjdbc.app.model.Label;
-import com.crudjdbc.app.repository.LabelRepository;
 import com.crudjdbc.app.repository.jdbc.JdbcLabelRepositoryImpl;
 import com.crudjdbc.app.service.LabelServiceImpl;
 import org.junit.jupiter.api.Test;
@@ -11,10 +10,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class LabelServiceTest {
@@ -34,12 +34,78 @@ public class LabelServiceTest {
         when(labelRepository.save(label)).thenReturn(label);
 
         labelRepository.save(label);
-        Label labelActual =labelServiceImpl.save(label);
+        Label labelActual = labelServiceImpl.save(label);
 
         assertNotNull(label);
         assertEquals(label, labelActual);
 
         verify(labelRepository).save(ArgumentMatchers.eq(label));
+    }
 
+    @Test
+    void deleteLabel_shouldBeSuccess() {
+        Label label = new Label();
+        label.setId(1);
+        label.setName("Test label");
+
+        doNothing().when(labelRepository).deleteById(label.getId());
+
+        labelRepository.deleteById(label.getId());
+
+        verify(labelRepository).deleteById(label.getId());
+    }
+
+    @Test
+    void updateLabel_shouldBeSuccess() {
+        Label labelExist = new Label();
+        Label labelUpdated = new Label();
+
+        labelExist.setId(1);
+        labelExist.setName("Test label");
+
+        labelUpdated.setName("Updated");
+        labelUpdated.setId(1);
+
+        lenient().when(labelRepository.save(labelExist)).thenReturn(labelExist);
+        lenient().when(labelRepository.update(labelUpdated)).thenReturn(labelUpdated);
+
+        Label result = labelRepository.update(labelUpdated);
+
+        assertNotNull(result);
+        assertNotEquals(labelExist.getName(), result.getName());
+
+        verify(labelRepository).update(labelUpdated);
+    }
+
+    @Test
+    void getLabelById_shouldBeSuccess() {
+        Label label = new Label();
+        label.setId(1);
+        label.setName("Test label");
+
+        when(labelRepository.getById(label.getId())).thenReturn(label);
+
+        Label result = labelRepository.getById(label.getId());
+
+        assertNotNull(result);
+        assertEquals(label, result);
+
+        verify(labelRepository).getById(label.getId());
+    }
+
+    @Test
+    void getAll_shouldBeSuccess() {
+        Label label = new Label();
+        label.setId(1);
+        label.setName("Test label");
+
+        List<Label> labels = new ArrayList<>();
+        labels.add(label);
+
+        when(labelRepository.getAll()).thenReturn(labels);
+
+        assertEquals(labels, labelRepository.getAll());
+
+        verify(labelRepository).getAll();
     }
 }
